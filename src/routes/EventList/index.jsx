18a973f489486b439a9aa3499b238a31a459526e
szz <shizengzhou@outlook.com>
2022-02-06 12:45:10 +0800

    解决多了一次请求的问题

--- src/routes/EventList/index.jsx
+++ src/routes/EventList/index.jsx
@@ -1,32 +1,32 @@
-import { useContext, useEffect, useState } from 'react';
+import { useContext, useEffect } from 'react';
 import { Link, useSearchParams } from 'react-router-dom';
 import { observer } from 'mobx-react-lite';
 import { Pagination } from 'antd';
 import EventCard from '../../components/EventCard';
 import { StoreContext } from '../../store';
 
-const EventList = observer(() =>{
-  const [searchParams] = useSearchParams();
-  const [current, setCurrent] = useState(1);
+const EventList = observer(() => {
+  const [searchParams, setSearchParams] = useSearchParams({ page: 1 });
   const store = useContext(StoreContext);
 
   useEffect(() => {
-    const page = searchParams.get('page');
-    console.log(page);
-    if (!page) {
-      setCurrent(1);
-    } else {
-      setCurrent(parseInt(page));
-    }
-    store.eventStore.fetchEvents(current);
-  }, [searchParams, store, current]);
+    store.eventStore.fetchEvents(Number(searchParams.get('page')));
+  }, [searchParams, store]);
 
   function renderPageItem(page, type) {
     if (type === 'prev') {
-      return <Link to={`/?page=${current - 1}`}>{type}</Link>
+      return (
+        <Link to={`/?page=${Number(searchParams.get('page')) - 1}`}>
+          {type}
+        </Link>
+      );
     }
     if (type === 'next') {
-      return <Link to={`/?page=${current + 1}`}>{type}</Link>
+      return (
+        <Link to={`/?page=${Number(searchParams.get('page')) + 1}`}>
+          {type}
+        </Link>
+      );
     }
     return <Link to={`/?page=${page}`}>{page}</Link>;
   }
@@ -40,10 +40,10 @@ const EventList = observer(() =>{
       <Pagination
         defaultPageSize={3}
         defaultCurrent={1}
-        current={current}
+        current={Number(searchParams.get('page'))}
         total={store.eventStore.eventsTotal}
         itemRender={renderPageItem}
-        onChange={page => setCurrent(page)}
+        onChange={page => setSearchParams({ page })}
       />
     </div>
   );
